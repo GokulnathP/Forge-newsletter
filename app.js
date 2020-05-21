7c43aa535a5458f4ce5df9a6e7cabe4df3410014
Gokulnath <4gokul2000@gmail.com>
2020-05-21 11:55:26 +0530

    data connection finished

--- app.js
+++ app.js
@@ -2,24 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
 const app = express();
 
-mongoose.connect("mongodb+srv://Username:password@cluster0-vmfvb.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Username:Password@cluster0-eme3k.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => { console.log("DB connected") }).catch((err) => { console.log(err) });
+
+const dataSchema = new mongoose.Schema({
+    name: "String",
+    team: "String"
+});
+
+var User = mongoose.model("User", dataSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static('views'));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    User.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index.ejs', { users: data });
+        }
+    })
+
 })
 
 app.post('/test', (req, res) => {
-    console.log(req.body);
+    var newUser = new User({
+        name: req.body.name,
+        team: req.body.team
+    }).save().then(saveData => console.log("data save", saveData)).catch((err) => console.log(err));
     res.redirect('/');
 })
 
